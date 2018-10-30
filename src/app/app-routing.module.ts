67f2cdf1477f1840f2474e732289c89aeb50ea13
liumeng2009 <378338627@qq.com>
2018-10-30 23:41:12 +0800

    201810302341
    201810302341

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import {OtherComponent} from './back/myComponents/noFound/other.component';
 import {NoAuthComponent} from './back/myComponents/noAuth/noAuth.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/default', pathMatch: 'full'},
-  {path: 'default', loadChildren: './front/default.module#DefaultModule'},
+  {path: '', loadChildren: './front/default.module#DefaultModule'},
   {path: 'admin/login', loadChildren: './back/login/login.module#LoginModule', data: {name: '登录'}},
   {path: 'admin', loadChildren: './back/main/main.module#MainModule', data: {name: '首页'}, canActivate: [TokenGuard]},
   {path: 'noauth', component: NoAuthComponent, data: {name: '没有权限访问该页面'}},
--- src/app/back/auth/user/user.component.ts
+++ src/app/back/auth/user/user.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-user-page',
@@ -7,13 +6,9 @@ import {Router} from '@angular/router';
   styleUrls: ['./user.component.scss']
 })
 
-export class UserComponent implements OnInit {
+export class UserComponent {
 
   constructor(
-    private router: Router,
-  ) {}
 
-  ngOnInit() {
-    this.router.navigateByUrl('/admin/auth/user/list');
-  }
+  ) {}
 }
