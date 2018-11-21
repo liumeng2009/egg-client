5c97e1d5c75e7402502b9c14280438e3b18ea937
liumeng <378338627@qq.com>
2018-11-21 16:03:29 +0800

    201811211603

--- src/app/front/default.component.ts
+++ src/app/front/default.component.ts
@@ -1,16 +1,14 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import algoliasearch from 'algoliasearch';
 import {AlgoliaConfig, AlgoliaQueryConfig} from '../config/algolia';
-import {fromEvent, Observable, Subject} from 'rxjs';
-import {catchError, debounceTime, distinctUntilChanged, switchMap, tap, map} from 'rxjs/operators';
-import {ResponseData} from '../bean/responseData';
-import {HttpHeaders} from '@angular/common/http';
-import {AuthService} from '../back/auth/auth.service';
+import {Observable, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-default-page',
   templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss']
+  styleUrls: ['./default.component.scss', '../../assets/css/dosearch.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class DefaultComponent implements OnInit {
@@ -19,83 +17,66 @@ export class DefaultComponent implements OnInit {
   client: any = {};
   index: any = {};
   packages$: Observable<any[]>;
-  searchText$ = new Subject<string>();
+  searchText$ = new Subject<any>();
   @ViewChild('search') searchInput;
+  showSearchBox = false;
   constructor(
-    private articleService: AuthService,
+
   ) {
     this.client = algoliasearch(
       'Y2MNOEONET',
       'a729eedd4490cfd5898cdc2f0bc672f9'
     );
     this.index = this.client.initIndex('demo_egg');
+    this.packages$ = this.searchText$.pipe(
+      debounceTime(500),
+    );
   }
 
   ngOnInit() {
-    console.log(123);
-/*    this.packages$ = this.searchText$.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      switchMap((value) => {
-        const str = this.searchFromAlgolia(value);
-        console.log(str);
-        return str;
-      })
-    );*/
-    this.searchText$.subscribe( (value) => {
-      console.log(value);
-    })
-    this.searchText$.pipe(
-      debounceTime(1000),
-      map( (value) => {
-        console.log(value);
-        return this.searchFromAlgolia(value);
-      })
-    );
-    const it = fromEvent(this.searchInput.nativeElement, 'keyup');
-    console.log(it);
-    const result = it.pipe(
-      debounceTime(1000),
-      distinctUntilChanged(),
-      switchMap((value) => {
-        console.log(value)
-        const str = this.searchFromAlgolia(value);
-        console.log(str);
-        return this.articleService.checkToken();
-      })
-    )
-    result.subscribe(x => console.log(x));
+    this.packages$.subscribe( (value) => {
+      this.searchFromAlgolia(value);
+    });
   }
 
-  searchFromAlgolia(value): Observable<any> {
-    console.log(value);
+  searchFromAlgolia(value) {
+    if (value === '') {
+      return;
+    }
+    const t = this;
     this.index.setSettings(AlgoliaConfig);
     this.index.batchRules(AlgoliaQueryConfig);
     this.index.search({query: value}, function searchDone(err, content) {
       if (err) {
         throw err;
       }
-      console.log(content.hits);
-      return content.hits;
+/*      for (const result of content.hits) {
+        t.options.push({title: result.title});
+      }*/
+      t.options = [...content.hits];
+      console.log(t.options);
+      t.showSearchBox = true;
+      // return content.hits;
     });
-    return null;
+    // return null;
   }
 
   onInput(value: string): void {
-    console.log(value);
     this.searchText$.next(value);
-/*    this.options = value ? [
-      value,
-      value + value,
-      value + value + value
-    ] : [];
-    this.index.setSettings(AlgoliaConfig);
-    this.index.batchRules(AlgoliaQueryConfig);
-    this.index.search({query: value}, function searchDone(err, content) {
-        if (err) {
-          throw err;
-        }
-        console.log(content.hits);
-    });*/
+  }
+
+  @ViewChild('search') public anchorOp: ElementRef;
+  @ViewChild('popupOp', { read: ElementRef }) public popupOp: ElementRef;
+  private contains(target: any): boolean {
+    return this.anchorOp.nativeElement.contains(target) ||
+      (this.popupOp ? this.popupOp.nativeElement.contains(target) : false);
+  }
+
+  @HostListener('document:click', ['$event'])
+  public documentClick(event: any): void {
+    console.log('click');
+    if (!this.contains(event.target)) {
+      this.showSearchBox = false;
+    }
   }
 }
